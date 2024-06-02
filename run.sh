649b66290f60885aa5d5490b9847e5966281f778
Henok Berhanu Tsegaye <henokberhanut@gmail.com>
2024-06-01 19:56:05 -0600

    updates

--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 docker compose up -d
+
+sleep 4
+docker exec -d  ue1 bash -c  "nr-ue --config uecfg.yaml &> ./ue1log.txt"
 sleep 3
-docker exec -d  ue1 bash -c  "nr-ue --config uecfg.yaml &> ./uelog.txt"
-sleep 2
-docker exec ue1 cat ./uelog.txt > ./logs/uelog.txt
-docker logs amf1 &> ./logs/amflog.txt
+docker exec ue1 cat ./ue1log.txt > ./logs/ue1log.txt
+
+sleep 4
+docker exec -d  ue2 bash -c  "nr-ue --config uecfg.yaml &> ./ue2log.txt"
+sleep 3
+docker exec ue2 cat ./ue2log.txt > ./logs/ue2log.txt
+
+docker logs amf1 &> ./logs/amf1log.txt
+sleep 4
+
 docker compose down
-echo "========================================LOG UE==============================="
-cat ./logs/uelog.txt
-echo "========================================LOG AMF=============================="
-cat ./logs/amflog.txt
+
+
+echo "========================================LOG UE1==============================="
+cat ./logs/ue1log.txt
+echo "========================================LOG UE2==============================="
+cat ./logs/ue2log.txt
+echo "========================================LOG AMF1=============================="
+cat ./logs/amf1log.txt
