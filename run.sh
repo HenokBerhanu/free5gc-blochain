40009d5996e6a7201cac527ae0b0b84ac9d9e0f3
Henok Berhanu Tsegaye <henokberhanut@gmail.com>
2024-06-01 19:44:08 -0600

    updates

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker compose up -d
 sleep 3
-docker exec -d  ue bash -c  "nr-ue --config uecfg.yaml &> ./uelog.txt"
+docker exec -d  ue1 bash -c  "nr-ue --config uecfg.yaml &> ./uelog.txt"
 sleep 2
-docker exec ue cat ./uelog.txt > ./logs/uelog.txt
-docker logs amf &> ./logs/amflog.txt
+docker exec ue1 cat ./uelog.txt > ./logs/uelog.txt
+docker logs amf1 &> ./logs/amflog.txt
 docker compose down
 echo "========================================LOG UE==============================="
 cat ./logs/uelog.txt
